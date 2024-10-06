893b6fbd091db87284b270ef7db1fed4e492f59b
Landen <ljbock68@gmail.com>
2024-10-06 11:45:27 -0500

    Image pathing

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ function displayInfo(data, pokemonName) {
         const listImg = document.createElement('img');
         
         if (typeClasses.hasOwnProperty(type)) {
-            listImg.src = "icons/Pokemon_Type_Icon_" + capitalizeFirstLetter(type) + ".png";
+            listImg.src = "https://ljokay.github.io/pokemonFinderJS/icons/Pokemon_Type_Icon_" + capitalizeFirstLetter(type) + ".png";
         }
            
         listImg.classList.add("li-img");
